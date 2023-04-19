d2169202e6e80629b27ce9d19a0be3565789ea95
Van Vu <vanvn.vu@gmail.com>
2023-04-19 10:00:22 +0700

    Fixes

--- bubble.ts
+++ bubble.ts
@@ -2,16 +2,17 @@ function bubbleSort(array: number[]) {
   for (let i = 0; i < array.length; i++) {
     for (let j = i + 1; j < array.length; j++) {
       if (array[j] < array[i]) {
-        let temp: number = array[j];
-        array[j] = array[i];
-        array[i] = temp;
+        // let temp: number = array[j];
+        // array[j] = array[i];
+        // array[i] = temp;
+        [array[i], array[j]] = [array[j], array[i]];
       }
     }
   }
   return array;
 }
 
-let testArray = [10, 11, 21, 412, 79, 90, 121, 56, 78, 2, 21, 6];
+// let testArray = [10, 11, 21, 412, 79, 90, 121, 56, 78, 2, 21, 6];
 let testArray2: number[] = [];
 for (let i = 0; i < 100; i++) {
   testArray2.push(Math.round(Math.random() * 100));
